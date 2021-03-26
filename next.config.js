52cdd8fbbc3187d07893248e819fc9ea6a55f5f5
hisamafahri <hisamafahri@gmail.com>
2021-03-26 09:33:44 +0700

    add distDir

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
-const nextEnv = require('next-env');
-const dotenvLoad = require('dotenv-load');
- 
+const nextEnv = require("next-env");
+const dotenvLoad = require("dotenv-load");
+
 dotenvLoad();
- 
+
 const withNextEnv = nextEnv();
- 
+
 module.exports = withNextEnv({
-  webpack: (config, options) => { config.module.rules.push({ parser: { amd: false } }) 
-  return config; },
-})
+  distDir: "build",
+  webpack: (config, options) => {
+    config.module.rules.push({ parser: { amd: false } });
+    return config;
+  },
+});
+
